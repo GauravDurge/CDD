020b48e6e481e41a2ec9c3f3de933d234872e278
GauravDurge <87854392+GauravDurge@users.noreply.github.com>
2021-07-25 17:57:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -26,10 +26,10 @@ from werkzeug.utils import secure_filename
 app = Flask(__name__)
 
 # Model saved with Keras model.save()
-MODEL_PATH ='model_inception.h5'
+#MODEL_PATH ='model_inception.h5'
 
 # Load your trained model
-model = load_model(MODEL_PATH)
+model = load_model('model_inception.h5')
 
 
 
